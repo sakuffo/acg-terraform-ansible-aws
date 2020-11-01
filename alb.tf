5844276b171e608a4e8296d01ca24a714abddc1e
Stephen Akuffo <12173427+sakuffo@users.noreply.github.com>
2020-11-01 10:50:38 -0500

    no ans | lb added

--- alb.tf
+++ alb.tf
@@ -1,10 +1,10 @@
-resource "aws_lb" "application-lb" {
+resource "aws_lb" "application_lb" {
   provider           = aws.region_primary
   name               = "jenkins-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb-sg.id]
-  subnets            = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.primary_subnet_1.id, aws_subnet.primary_subnet_2.id]
   tags = {
     Name = "Jenkins-LB"
   }
@@ -13,18 +13,29 @@ resource "aws_lb" "application-lb" {
 resource "aws_lb_target_group" "app_lb_tg" {
   provider = aws.region_primary
   name     = "app-lb-tg"
-  port     = "instance"
+  port     = var.webserver_port
   vpc_id   = aws_vpc.vpc_primary.id
   protocol = "HTTP"
   health_check {
     enabled  = true
     interval = 10
     path     = "/"
-    port     = var.webserver-port
+    port     = var.webserver_port
     protocol = "HTTP"
     matcher  = "200-299"
   }
   tags = {
     Name = "jenkins-target-group"
+  }
+}
+
+resource "aws_lb_listener" "jenkins_listener_http" {
+  provider = aws.region_primary
+  load_balancer_arn = aws_lb.application_lb.arn
+  port = "80"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.app_lb_tg.id
   }
 }
