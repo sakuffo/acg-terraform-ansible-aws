382bf7ed14f41a30453f149e8ab7805577dc6907
Stephen Akuffo <12173427+sakuffo@users.noreply.github.com>
2020-11-01 15:37:42 -0500

    testing ans fix

--- network.tf
+++ network.tf
@@ -22,51 +22,6 @@ resource "aws_vpc" "vpc_secondary" {
   }
 }
 
-## Ceate IGWs ##
-
-#Create IGW in us-east-1
-resource "aws_internet_gateway" "igw_primary" {
-  provider = aws.region_primary
-  vpc_id   = aws_vpc.vpc_primary.id
-}
-#Create IGW in us-west-2
-resource "aws_internet_gateway" "igw_secondary" {
-  provider = aws.region_secondary
-  vpc_id   = aws_vpc.vpc_secondary.id
-}
-
-## Ceate subnets ##
-
-#Get all available AZ's in VPC for primary region
-data "aws_availability_zones" "azs" {
-  provider = aws.region_primary
-  state    = "available"
-}
-
-#Create subnet # 1 in us-east-1
-resource "aws_subnet" "primary_subnet_1" {
-  provider          = aws.region_primary
-  availability_zone = element(data.aws_availability_zones.azs.names, 0)
-  vpc_id            = aws_vpc.vpc_primary.id
-  cidr_block        = "10.10.1.0/24"
-}
-
-#Create subnet # 2 in us-east-1
-resource "aws_subnet" "primary_subnet_2" {
-  provider          = aws.region_primary
-  availability_zone = element(data.aws_availability_zones.azs.names, 1)
-  vpc_id            = aws_vpc.vpc_primary.id
-  cidr_block        = "10.10.2.0/24"
-}
-#Create subnet # 1 in us-west-1
-resource "aws_subnet" "secondary_subnet_1" {
-  provider = aws.region_secondary
-
-  vpc_id     = aws_vpc.vpc_secondary.id
-  cidr_block = "192.10.1.0/24"
-}
-
-
 ## Create Peering connection request ##
 
 #Initiate Peering connection request from us-east-1 to us-west-2
@@ -83,6 +38,19 @@ resource "aws_vpc_peering_connection_accepter" "secondary_accept_peering" {
   provider                  = aws.region_secondary
   vpc_peering_connection_id = aws_vpc_peering_connection.primary_to_secondary.id
   auto_accept               = true
+}
+
+## Ceate IGWs ##
+
+#Create IGW in us-east-1
+resource "aws_internet_gateway" "igw_primary" {
+  provider = aws.region_primary
+  vpc_id   = aws_vpc.vpc_primary.id
+}
+#Create IGW in us-west-2
+resource "aws_internet_gateway" "igw_secondary" {
+  provider = aws.region_secondary
+  vpc_id   = aws_vpc.vpc_secondary.id
 }
 
 ## Create routing tables ##
@@ -141,3 +109,38 @@ resource "aws_main_route_table_association" "set-secondary-default-rt-assoc" {
   vpc_id         = aws_vpc.vpc_secondary.id
   route_table_id = aws_route_table.internet_route_secondary.id
 }
+
+## Ceate subnets ##
+
+#Get all available AZ's in VPC for primary region
+data "aws_availability_zones" "azs" {
+  provider = aws.region_primary
+  state    = "available"
+}
+
+#Create subnet # 1 in us-east-1
+resource "aws_subnet" "primary_subnet_1" {
+  provider          = aws.region_primary
+  availability_zone = element(data.aws_availability_zones.azs.names, 0)
+  vpc_id            = aws_vpc.vpc_primary.id
+  cidr_block        = "10.10.1.0/24"
+}
+
+#Create subnet # 2 in us-east-1
+resource "aws_subnet" "primary_subnet_2" {
+  provider          = aws.region_primary
+  availability_zone = element(data.aws_availability_zones.azs.names, 1)
+  vpc_id            = aws_vpc.vpc_primary.id
+  cidr_block        = "10.10.2.0/24"
+}
+#Create subnet # 1 in us-west-1
+resource "aws_subnet" "secondary_subnet_1" {
+  provider = aws.region_secondary
+
+  vpc_id     = aws_vpc.vpc_secondary.id
+  cidr_block = "192.10.1.0/24"
+}
+
+
+
+
